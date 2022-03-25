2d0eb2eb3bc29f2433507d8a78821020a9b787fe
Juliana Vaz da Silva <vazjuliana1993@gmail.com>
2022-03-24 21:24:51 -0300

    cabeçalho com papel de fundo

--- app/src/components/cabecalho/cabecalho.js
+++ app/src/components/cabecalho/cabecalho.js
@@ -1,13 +1,30 @@
-import {
-    Component 
-} from "react";
+import {Component} from "react";
+import './Cabecalho.css';
 
-class Cabecalho extends Component {
+
+export class Cabecalho extends Component {
     constructor(props){
         super(props)
     }
 
     render(){
-        return ()
+        return (
+          <header class="cabecalho">
+          <nav class="cabecalho-menu">
+              <a class="cabecalho-menu-item">Skills </a>
+              <a class="cabecalho-menu-item">Projetos</a>
+              <a class="cabecalho-menu-item">Formação</a>
+              <a class="cabecalho-menu-item">Contatos</a>
+          </nav>
+          <div class="cabecalho-bloco-dois">
+            <div class="cabecalho-bloco-dois-esquerda">
+              <h1>Juliana Vaz da Silva</h1>
+              <h2>Desenvolvedora Front-End</h2>
+            </div>
+              <img class="cabecalho-foto" src="imagens/minhaImagem.jpg"/>
+          </div>
+      </header>
+           
+        )
     }
 }
